a7b60eae9241e55178fd700d2123ca3876d7bd78
2023bit506 <2023bit506@sggs.ac.in>
2024-03-21 23:49:45 +0530

    String

---
+++ String2.java
@@ -0,0 +1,12 @@
+public class String2 {
+    public static void main(String[] args) {
+        String s1 = "Shubham";
+        String s2 = "Shubham";
+        String s3 = new String("Pawar"); 
+        String s4 = new String("Pawar"); 
+        System.out.println(s1==s2);
+        System.out.println(s3==s4);
+    
+    }
+    
+}
