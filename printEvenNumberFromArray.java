4642fa22f37a0a5cd00c9ed31dd2d4d339c7db95
2023bit506 <2023bit506@sggs.ac.in>
2024-03-27 14:55:54 +0530

    Printing all even numbers from an array

---
+++ printEvenNumberFromArray.java
@@ -0,0 +1,19 @@
+public class printEvenNumberFromArray {
+
+    public void displayEvenNumbers(int[] arr)
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]%2==0)
+            {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        printEvenNumberFromArray e1 = new printEvenNumberFromArray(); 
+        int[] arr = {1,2,3,4,5,6};
+        e1.displayEvenNumbers(arr);
+
+    }
+}
