5f89d62473081de2fe95933dcd0b2b4b5ba6f3bc
2023bit506 <2023bit506@sggs.ac.in>
2024-03-30 18:58:05 +0530

    Static block practice

---
+++ Rough3.java
@@ -0,0 +1,20 @@
+public class Rough3 {
+    static
+    {
+        System.out.println("Hello Shubham");
+    }
+    {
+        System.out.println("Hello SGGS");
+    }
+    public static void main(String[] args)
+    {
+        Rough3 s1 = new Rough3();
+
+    }
+    static{
+        System.out.println("Hello SGGS 3");
+    }
+    {
+        System.out.println("Hello SGGS 4");
+    }
+}
